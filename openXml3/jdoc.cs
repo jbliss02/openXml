a437c09045fa04882b40872896140bb122fec6ab
jbliss02 <jbliss02@dcs.bbk.ac.uk>
2015-01-12 22:12:28 +0000

    latest

--- openXml3/jdoc.cs
+++ openXml3/jdoc.cs
@@ -30,10 +30,10 @@ namespace openXml3
         public void createDoc()
         {
             // Create a Wordprocessing document. 
-            using (WordprocessingDocument myDoc = WordprocessingDocument.Create(_documentName,WordprocessingDocumentType.Document))
+            using (WordprocessingDocument wordDoc = WordprocessingDocument.Create(_documentName,WordprocessingDocumentType.Document))
             {
                 // Add a new main document part. 
-                mainPart = myDoc.AddMainDocumentPart();
+                mainPart = wordDoc.AddMainDocumentPart();
                 mainPart.Document = new Document();
                 
                 //Create Body (this element contains other elements that we want to include 
@@ -43,16 +43,27 @@ namespace openXml3
                 addExistingStyles();
                 //addNewStyle(mainPart);
 
+                pleasework();
+
+                body.Append(returnTextInput());
+                body.Append(returnTextInput());
+
                 addSectionHeader("Users and user limits");
                 addSectionText("This is where the random text comes, blah blah blah");
+                addSectionText("Another line.");
+                addSectionBreak(SectionMarkValues.Continuous);
+                addSectionText("Another line.");
                 addTable();
-
-                //table
+                addTextBox();
+              
+                ////table
                 Table table = new Table(new TableRow(new TableCell(new Paragraph(new Run(new Text("Hello World!"))))));
                 body.Append(table);
+                mainPart.Document.Append(body);
 
                 setMargins();
-
+                addHeader("THE HEADING!");
+                addFooter("txt");
                 mainPart.Document.Save();
             } 
         }//createDoc
@@ -78,14 +89,41 @@ namespace openXml3
         private void addSectionText(string text)
         {
             //Create paragraph 
+       
             Paragraph paragraph = new Paragraph();
             Run run_paragraph = new Run();
             Text text_paragraph = new Text(text);
             run_paragraph.Append(text_paragraph);
             paragraph.Append(run_paragraph);
+
+            paragraph.AppendChild(new Paragraph(new ParagraphProperties(new SectionProperties(new SectionType() { Val = SectionMarkValues.Continuous }))));
+
             body.Append(paragraph);
-            mainPart.Document.Append(body);
+
             // Save changes to the main document part. 
+        }
+
+        private void addTextBox()
+        {
+
+            FormFieldData fd = new FormFieldData();
+            TextInput txt = new TextInput();
+        
+            fd.AppendChild(txt);
+
+            Paragraph paragraph = new Paragraph();
+            Run run_paragraph = new Run();
+            run_paragraph.Append(fd);
+            paragraph.Append(run_paragraph);
+            body.Append(paragraph);
+
+;
+
+            return;
+
+            BookmarkStart bkmStart = new BookmarkStart() { Name = "Table1", Id = "Table1" };
+            BookmarkEnd bkmEnd = new BookmarkEnd() { Id = "Table1" };
+            body.Append(bkmStart);
         }
 
         private void addTable()
@@ -94,11 +132,12 @@ namespace openXml3
            
             TableProperties tblPr = new TableProperties();
             tblPr.TableStyle = new TableStyle();
-            tblPr.TableStyle.Val = "OneContractNoBorder";
+            tblPr.TableStyle.Val = "ocSummary";
 
-
+            TableWidth tableWidth = new TableWidth() { Width = "5000", Type = TableWidthUnitValues.Pct };
             TableBorders tblBorders = new TableBorders();
 
+            #region ramblings
             //tblBorders.TopBorder = new TopBorder();
             //tblBorders.TopBorder.Val = new EnumValue<BorderValues>(BorderValues.Single);
             //tblBorders.BottomBorder = new BottomBorder();
@@ -111,7 +150,8 @@ namespace openXml3
             //tblBorders.InsideHorizontalBorder.Val = BorderValues.Single;
             //tblBorders.InsideVerticalBorder = new InsideVerticalBorder();
             //tblBorders.InsideVerticalBorder.Val = BorderValues.Single;
-            tblPr.Append(tblBorders);
+            #endregion
+            tblPr.Append(tblBorders, tableWidth);
             table.Append(tblPr);
             TableRow tr;
             TableCell tc;
@@ -142,7 +182,8 @@ namespace openXml3
                 tr.Append(new TableCell(new Paragraph(new Run(new Text(i.ToString())))));
                 for (int j = 1; j <= 10; j++)
                 {
-                    tr.Append(new TableCell(new Paragraph(new Run(new Text((i * j).ToString())))));
+                    //tr.Append(new TableCell(new Paragraph(new Run(new Text((i * j).ToString())))));
+                    tr.Append(new TableCell(returnTextInput()));
                 }
                 table.Append(tr);
             }
@@ -153,6 +194,256 @@ namespace openXml3
 
 
 
+        }
+
+        private void addFooter(string text)
+        {
+            //footer
+            FooterPart newFtPart = mainPart.AddNewPart<FooterPart>();
+            string ft_ID = mainPart.GetIdOfPart(newFtPart);
+
+            returnFooter(text).Save(newFtPart);
+            foreach (SectionProperties sectProperties in mainPart.Document.Descendants<SectionProperties>())
+            {
+                FooterReference newFtReference =
+                 new FooterReference() { Id = ft_ID, Type = HeaderFooterValues.Default };
+                sectProperties.Append(newFtReference);
+            }
+
+        }//addFooter()
+
+        private void addHeader(string text)
+        {
+            HeaderPart newFtPart = mainPart.AddNewPart<HeaderPart>();
+            string ft_ID = mainPart.GetIdOfPart(newFtPart);
+
+            returnHeader(text).Save(newFtPart);
+            foreach (SectionProperties sectProperties in mainPart.Document.Descendants<SectionProperties>())
+            {
+                HeaderReference newFtReference =
+                 new HeaderReference() { Id = ft_ID, Type = HeaderFooterValues.Default };
+                sectProperties.Append(newFtReference);
+            }
+
+
+        }//addHeader()
+
+        private void addSectionBreak(SectionMarkValues breakType)
+        {
+            //has to be appended to a paragraph, otherwise is a page break
+
+            //body.AppendChild(new Paragraph(new Run(new SectionProperties( new SectionType() { Val = SectionMarkValues.Continuous }))));
+
+            body.AppendChild(new Paragraph(new ParagraphProperties(new SectionProperties(new SectionType() { Val = SectionMarkValues.Continuous }))));
+
+            return;
+
+
+            Paragraph p = new Paragraph();
+
+            ParagraphProperties p_prop = new ParagraphProperties();
+
+            SectionProperties sectionProperties1 = new SectionProperties();
+            SectionType sectionType1 = new SectionType() { Val = SectionMarkValues.Continuous };
+
+            sectionProperties1.Append(sectionType1);
+
+            p_prop.Append(sectionProperties1);
+
+            p.Append(p_prop);
+
+            Run run_paragraph = new Run();
+            Text text_paragraph = new Text("-");
+            run_paragraph.Append(text_paragraph);
+            p.Append(run_paragraph);
+
+
+            body.Append(p);
+        }
+
+        private void addCustomerBox()
+        {
+            XmlDocument custXml = new XmlDocument();
+            custXml.Load(@"c:\temp\customerTemplate.xml");
+
+            using (Stream outputStream = mainPart.GetStream())
+            {
+                using (StreamWriter ts = new StreamWriter(outputStream))
+                {
+                    ts.Write(custXml.InnerXml);
+                }
+            }
+
+
+        }
+
+        private Paragraph returnTextInput()
+        {
+            Paragraph paragraph1 = new Paragraph() { RsidParagraphAddition = "00A079F1", RsidRunAdditionDefault = "008E35FE" };
+
+            Run run1 = new Run();
+
+            FieldChar fieldChar1 = new FieldChar() { FieldCharType = FieldCharValues.Begin };
+
+            FormFieldData formFieldData1 = new FormFieldData();
+            FormFieldName formFieldName1 = new FormFieldName() { Val = "Text35" };
+            Enabled enabled1 = new Enabled();
+            CalculateOnExit calculateOnExit1 = new CalculateOnExit() { Val = false };
+            TextInput textInput1 = new TextInput();
+
+            formFieldData1.Append(formFieldName1);
+            formFieldData1.Append(enabled1);
+            formFieldData1.Append(calculateOnExit1);
+            formFieldData1.Append(textInput1);
+
+            fieldChar1.Append(formFieldData1);
+
+            run1.Append(fieldChar1);
+
+            Run run2 = new Run();
+            FieldCode fieldCode1 = new FieldCode() { Space = SpaceProcessingModeValues.Preserve };
+            fieldCode1.Text = " FORMTEXT ";
+
+            run2.Append(fieldCode1);
+
+            Run run3 = new Run();
+            FieldChar fieldChar2 = new FieldChar() { FieldCharType = FieldCharValues.Separate };
+
+            run3.Append(fieldChar2);
+
+            Run run4 = new Run();
+
+            RunProperties runProperties1 = new RunProperties();
+            NoProof noProof1 = new NoProof();
+
+            runProperties1.Append(noProof1);
+            Text text1 = new Text();
+            text1.Text = " ";
+
+            run4.Append(runProperties1);
+            run4.Append(text1);
+
+            Run run5 = new Run();
+
+            RunProperties runProperties2 = new RunProperties();
+            NoProof noProof2 = new NoProof();
+
+            runProperties2.Append(noProof2);
+            Text text2 = new Text();
+            text2.Text = " ";
+
+            run5.Append(runProperties2);
+            run5.Append(text2);
+
+            Run run6 = new Run();
+
+            RunProperties runProperties3 = new RunProperties();
+            NoProof noProof3 = new NoProof();
+
+            runProperties3.Append(noProof3);
+            Text text3 = new Text();
+            text3.Text = " ";
+
+            run6.Append(runProperties3);
+            run6.Append(text3);
+
+            Run run7 = new Run();
+
+            RunProperties runProperties4 = new RunProperties();
+            NoProof noProof4 = new NoProof();
+
+            runProperties4.Append(noProof4);
+            Text text4 = new Text();
+            text4.Text = " ";
+
+            run7.Append(runProperties4);
+            run7.Append(text4);
+
+            Run run8 = new Run();
+
+            RunProperties runProperties5 = new RunProperties();
+            NoProof noProof5 = new NoProof();
+
+            runProperties5.Append(noProof5);
+            Text text5 = new Text();
+            text5.Text = " ";
+
+            run8.Append(runProperties5);
+            run8.Append(text5);
+
+            Run run9 = new Run();
+            FieldChar fieldChar3 = new FieldChar() { FieldCharType = FieldCharValues.End };
+
+            run9.Append(fieldChar3);
+            BookmarkStart bookmarkStart1 = new BookmarkStart() { Name = "_GoBack", Id = "0" };
+            BookmarkEnd bookmarkEnd1 = new BookmarkEnd() { Id = "0" };
+
+            paragraph1.Append(run1);
+            paragraph1.Append(run2);
+            paragraph1.Append(run3);
+            paragraph1.Append(run4);
+            paragraph1.Append(run5);
+            paragraph1.Append(run6);
+            paragraph1.Append(run7);
+            paragraph1.Append(run8);
+            paragraph1.Append(run9);
+            paragraph1.Append(bookmarkStart1);
+            paragraph1.Append(bookmarkEnd1);
+
+            //body.Append(paragraph1);
+            return paragraph1;
+        }
+
+        private void pleasework()
+        {
+            Paragraph paragraph1 = new Paragraph();
+
+            SimpleField simpleField = new SimpleField(); simpleField.Instruction = "FORMTEXT";
+
+            FormFieldData formFieldData = new FormFieldData();
+
+            formFieldData.Append(new FormFieldName() { Val = "MyField" });
+
+            formFieldData.Append(new Enabled() { Val = OnOffValue.FromBoolean(false) });
+
+            TextInput textInput = new TextInput();
+
+            textInput.Append(new MaxLength() { Val = 4 });
+            formFieldData.Append(textInput);
+            simpleField.Append(formFieldData);
+            paragraph1.Append(simpleField);
+            body.Append(paragraph1);
+
+        }
+
+        private Footer returnFooter(string text)
+        {
+            var element =
+              new Footer(
+                new Paragraph(
+                  new ParagraphProperties(
+                    new ParagraphStyleId() { Val = "Footer" }),
+                  new Run(
+                    new Text(text))
+                )
+              );
+
+            return element;
+        }
+
+        private Header returnHeader(string text)
+        {
+            var element =
+              new Header(
+                new Paragraph(
+                  new ParagraphProperties(
+                    new ParagraphStyleId() { Val = "Header" }),
+                  new Run(
+                    new Text(text))
+                )
+  );
+
+            return element;
         }
 
         private void setMargins()
